14c1810e02c7806506c916b4f04750bc12db69a6
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-28 14:43:40 -0400

    agregue datos json para la UI de artistas

---
+++ src/constants/popularAlbums.ts
@@ -0,0 +1,34 @@
+import { PopularAlbums } from "../types/index.d";
+
+export const popularAlbums: PopularAlbums[] = [
+  {
+    id: "1",
+    albumName: "Un verano sin ti",
+    artistName: "Bad Bunny",
+    albumPhoto: "/public/img/popular-albums/unveranosinti.jpeg",
+  },
+  {
+    id: "2",
+    albumName: "Manana sera bonito",
+    artistName: "Karol G",
+    albumPhoto: "/public/img/popular-albums/mananasera.jpeg",
+  },
+  {
+    id: "3",
+    albumName: "Ferxxocalipsis",
+    artistName: "Feid",
+    albumPhoto: "/public/img/popular-albums/ferxxocalipsis.jpeg",
+  },
+  {
+    id: "4",
+    albumName: "Super Cumbias",
+    artistName: "Los Angeles Azules",
+    albumPhoto: "/public/img/popular-albums/angelesazules.jpeg",
+  },
+  {
+    id: "5",
+    albumName: "El comienzo",
+    artistName: "Grupo frontera",
+    albumPhoto: "/public/img/popular-albums/elcomienzo.jpeg",
+  },
+];
---
+++ src/constants/popularArtists.ts
@@ -0,0 +1,39 @@
+import { PopularArtists } from "../types/index.d";
+
+export const popularArtist: PopularArtists[] = [
+  {
+    id: "1",
+    name: "Feid",
+    photo: "/public/img/popular-artists/feid.jpeg",
+  },
+  {
+    id: "2",
+    name: "Karol G",
+    photo: "/public/img/popular-artists/karolg.jpeg",
+  },
+  {
+    id: "3",
+    name: "Bad Bunny",
+    photo: "/public/img/popular-artists/badbunny.jpeg",
+  },
+  {
+    id: "4",
+    name: "Grupo Frontera",
+    photo: "/public/img/popular-artists/grupofrontera.jpeg",
+  },
+  {
+    id: "5",
+    name: "Maluma",
+    photo: "/public/img/popular-artists/maluma.jpeg",
+  },
+  {
+    id: "6",
+    name: "Los Angeles Azules",
+    photo: "/public/img/popular-artists/losangelesazules.jpeg",
+  },
+  {
+    id: "7",
+    name: "Myke Towers",
+    photo: "/public/img/popular-artists/myketowers.jpeg",
+  },
+];
