e7de8bc16e5f66d6838fe99731cb147be9e1f376
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-28 16:52:11 -0400

    agregue contendor de lista para el componente CoverCard

---
+++ src/components/shared/CoverList.tsx
@@ -0,0 +1,27 @@
+import CoverCard from "./CoverCard";
+import { CoverListProps } from "../../types";
+import { Fragment } from "react/jsx-runtime";
+
+export default function CoverList({ title, list, type }: CoverListProps) {
+  return (
+    <section className="w-[90%] m-auto mt-14">
+      <h2 className="text-white font-extrabold text-3xl">{title}</h2>
+      <article
+        className="w-full h-full mt-5 grid grid-cols-[minmax(168px,1fr)] auto-cols-[minmax(168px,1fr)] grid-flow-col overflow-x-scroll gap-5"
+        id="cover-list"
+      >
+        {list.map((item) => (
+          <Fragment key={item.id}>
+            {type === "artist" && "artistName" in item && (
+              <CoverCard artistName={item.artistName} photo={item.photo} />
+            )}
+
+            {type === "album" && "albumName" in item && (
+              <CoverCard albumName={item.albumName} photo={item.photo} />
+            )}
+          </Fragment>
+        ))}
+      </article>
+    </section>
+  );
+}
