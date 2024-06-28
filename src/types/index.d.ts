566cdbf6b3e6b934b5986b119a04a9f97be1263e
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-28 14:44:47 -0400

    agregue tipos para popularArtists y popularAlbums

---
+++ src/types/index.d.ts
@@ -0,0 +1,12 @@
+export interface PopularArtists {
+  id: string;
+  name: string;
+  photo: string;
+}
+
+export interface PopularAlbums {
+  id: string;
+  albumName: string;
+  artistName: string;
+  albumPhoto: string;
+}
