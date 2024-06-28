60068281a38abe3717a9709500944527cf81f31a
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-28 16:48:08 -0400

    omision de la ruta inicial public al comienzo de las rutas

--- src/constants/popularArtists.ts
+++ src/constants/popularArtists.ts
@@ -3,37 +3,37 @@ import { PopularArtists } from "../types/index.d";
 export const popularArtist: PopularArtists[] = [
   {
     id: "1",
-    name: "Feid",
-    photo: "/public/img/popular-artists/feid.jpeg",
+    artistName: "Feid",
+    photo: "img/popular-artists/feid.jpeg",
   },
   {
     id: "2",
-    name: "Karol G",
-    photo: "/public/img/popular-artists/karolg.jpeg",
+    artistName: "Karol G",
+    photo: "img/popular-artists/karolg.jpeg",
   },
   {
     id: "3",
-    name: "Bad Bunny",
-    photo: "/public/img/popular-artists/badbunny.jpeg",
+    artistName: "Bad Bunny",
+    photo: "img/popular-artists/badbunny.jpeg",
   },
   {
     id: "4",
-    name: "Grupo Frontera",
-    photo: "/public/img/popular-artists/grupofrontera.jpeg",
+    artistName: "Grupo Frontera",
+    photo: "img/popular-artists/grupofrontera.jpeg",
   },
   {
     id: "5",
-    name: "Maluma",
-    photo: "/public/img/popular-artists/maluma.jpeg",
+    artistName: "Maluma",
+    photo: "img/popular-artists/maluma.jpeg",
   },
   {
     id: "6",
-    name: "Los Angeles Azules",
-    photo: "/public/img/popular-artists/losangelesazules.jpeg",
+    artistName: "Los Angeles Azules",
+    photo: "img/popular-artists/losangelesazules.jpeg",
   },
   {
     id: "7",
-    name: "Myke Towers",
-    photo: "/public/img/popular-artists/myketowers.jpeg",
+    artistName: "Myke Towers",
+    photo: "img/popular-artists/myketowers.jpeg",
   },
 ];
