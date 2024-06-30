b4d93bf38ab0ef8650f81a1e15b4bba29d2a85cb
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-29 23:16:38 -0400

    Agregue redux toolkit y el componente CoverCardDetails

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import Layout from "./components/views/Layout";
 import Home from "./components/views/Home";
 import "./index.css";
+import CoverCardDetails from "./components/views/CoverCardDetails";
 
 const router = createBrowserRouter([
   {
@@ -14,12 +17,19 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "artist/:id",
+        element: <CoverCardDetails />,
+        loader: ({ params }) => params,
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
