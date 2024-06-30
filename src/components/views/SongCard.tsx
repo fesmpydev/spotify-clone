ff43dbc377a1b57b566bd186a19aecdea56b4663
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-29 23:25:36 -0400

    Agregue el componente SongCard para mostrar detalles de la cancion

---
+++ src/components/views/SongCard.tsx
@@ -0,0 +1,42 @@
+import PlayButton from "../shared/PlayButton";
+
+export default function SongCard({
+  name,
+  cover,
+  artistId,
+  songId,
+}: {
+  name: string;
+  cover: string;
+  artistId: string;
+  songId: string;
+}) {
+  return (
+    <article className="w-full h-full flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <PlayButton
+          cover={cover}
+          name={name}
+          artistId={artistId}
+          songId={songId}
+        />
+        <div>
+          <h3 className="text-white text-3xl font-semibold">{name}</h3>
+          <span className="text-white text-xl font-medium">5.000.000</span>
+        </div>
+      </div>
+      <span>
+        <svg
+          data-encore-id="icon"
+          role="img"
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          style={{ fill: "#fff", width: "26" }}
+          className="Svg-sc-ytk21e-0 bneLcE"
+        >
+          <path d="M10.5 4.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0zm0 15a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0zm0-7.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0z"></path>
+        </svg>
+      </span>
+    </article>
+  );
+}
