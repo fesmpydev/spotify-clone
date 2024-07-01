6b3eb3d37ead44e9405f60121268c71010f808b0
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-30 22:33:22 -0400

    Inserte logica condicional dependiendo del tipo de contenido(album o artist)

--- src/components/views/CoverCardDetails.tsx
+++ src/components/views/CoverCardDetails.tsx
@@ -1,40 +1,111 @@
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import usePlayButton from "../../hooks/usePlayButton";
 import SongCard from "./SongCard";
 import useCoverCardDetails from "../../hooks/useCoverCardDetails";
 
 export default function CoverCardDetails() {
+  const [totalPlayTime, setTotalPlayTime] = useState(0);
   const params = useLoaderData() as { id: string };
   const { handlePlayFirstSong } = usePlayButton();
   const { pathname } = window.location;
   const typeCover = pathname.split("/")[1];
 
-  const { data } = useCoverCardDetails({ type: typeCover, id: params.id });
+  const { albumData, artistData } = useCoverCardDetails({
+    type: typeCover,
+    id: params.id,
+  });
+
+  useEffect(() => {
+    if (albumData) {
+      (async () => {
+        const time = await albumData.songsTotalTime;
+        setTotalPlayTime(time);
+      })();
+    }
+  }, [albumData]);
+
+  useEffect(() => {
+    const header = window.document.getElementById("header");
+
+    if (header) {
+      header.style.backgroundColor = "#1db954";
+    }
+
+    return () => {
+      if (header) {
+        header.style.backgroundColor = "#000";
+      }
+    };
+  }, []);
+
+  const data = albumData ? albumData : artistData;
 
   return (
     <section className="w-full min-h-screen mb-20">
       <article
-        className={`w-full h-[50vh] bg-gradient-to-b from-${data?.bgColor}-500 to-slate-700 grid place-items-center`}
+        className={`w-full h-auto mb-3 bg-gradient-to-b from-green-500 to-black grid place-items-center`}
       >
         <img
           src={data?.photo}
           alt={data?.artistName}
-          className="w-[150px] h-[150px] rounded-full object-cover m-auto mt-10"
+          className={`w-[150px] h-[150px] ${
+            typeCover === "artist" ? "rounded-full" : ""
+          } object-cover m-auto mt-10`}
         />
         <div className="w-[90%] m-auto mt-14">
           <h2 className="text-white font-extrabold text-5xl">
-            {data?.artistName}
+            {artistData?.artistName || albumData?.albumName}
           </h2>
-          <span className="text-slate-200 font-medium mt-4 block text-2xl">
-            {data?.listeners} monthly listeners
+          {typeCover === "album" && (
+            <div className="flex items-center gap-5 mt-5">
+              <img
+                src={albumData?.artistPhoto}
+                alt={albumData?.artistName}
+                className="w-[28px] h-[28px] rounded-full object-cover"
+              />
+              <span className="text-white font-bold text-xl">
+                {albumData?.artistName}
+              </span>
+            </div>
+          )}
+          <span className="text-slate-200 font-medium mt-4 text-2xl flex items-center gap-3">
+            {typeCover === "album" ? (
+              <>
+                <span>{albumData?.year}</span>
+                <span>• {albumData?.songsQty} songs,</span>
+                <span> {totalPlayTime} min</span>
+              </>
+            ) : (
+              <span>{artistData?.listeners} listeners</span>
+            )}
           </span>
         </div>
       </article>
 
       <article className="w-[90%] m-auto flex items-center justify-between">
         <div className="flex items-center gap-10">
-          <button className="rounded-full p-3 border border-white w-[70px]">
-            <span className="text-white font-extrabold text-xl">Follow</span>
+          <button
+            className={`${
+              typeCover === "album"
+                ? ""
+                : "rounded-full p-3 border border-white w-[70px]"
+            }`}
+          >
+            {typeCover === "album" ? (
+              <svg
+                data-encore-id="icon"
+                role="img"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                style={{ fill: "#fff", width: "24px", height: "24px" }}
+                className="Svg-sc-ytk21e-0 bneLcE"
+              >
+                <path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path>
+              </svg>
+            ) : (
+              <span className="text-white font-extrabold text-xl">Follow</span>
+            )}
           </button>
           <span>
             <svg
@@ -65,8 +136,8 @@ export default function CoverCardDetails() {
           className="w-[50px] h-[50px] rounded-full grid place-items-center bg-green-500"
           onClick={() =>
             handlePlayFirstSong({
-              artistId: data!.id,
-              songId: data?.songs ? data.songs[0].id : undefined,
+              artistId: artistData!.id,
+              songId: artistData?.songs ? artistData.songs[0].id : undefined,
             })
           }
         >
@@ -84,18 +155,30 @@ export default function CoverCardDetails() {
       </article>
 
       <article className="w-[90%] m-auto mt-14">
-        <h2 className="text-white font-extrabold text-3xl">Popular</h2>
+        {typeCover === "artist" && (
+          <h2 className="text-white font-extrabold text-3xl">Popular</h2>
+        )}
 
         <section className="mt-5 flex flex-col gap-10">
-          {data?.songs?.map((song) => (
-            <SongCard
-              key={song.name}
-              name={song.name}
-              cover={song.cover}
-              artistId={params.id}
-              songId={song.id}
-            />
-          ))}
+          {typeCover === "artist"
+            ? artistData?.songs?.map((song) => (
+                <SongCard
+                  key={song.name}
+                  name={song.name}
+                  cover={song.cover}
+                  artistId={params.id}
+                  songId={song.id}
+                />
+              ))
+            : albumData?.songs?.map((song) => (
+                <SongCard
+                  key={song.id}
+                  name={song.name}
+                  cover={song.cover}
+                  artistId={params.id}
+                  songId={song.id}
+                />
+              ))}
         </section>
       </article>
     </section>
