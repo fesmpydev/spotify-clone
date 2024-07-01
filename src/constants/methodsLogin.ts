c6c6f5a1363b71a6f5b3dc0c7b198374aaaeb2b6
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-30 22:37:24 -0400

    Agregue constante de array para las formas de inicio de sesion

---
+++ src/constants/methodsLogin.ts
@@ -0,0 +1,7 @@
+import { MethodsLogin } from "../types";
+
+export const methodsLogin: MethodsLogin[] = [
+  { id: 1, name: "Continue with Facebook", icon: "/icon/facebook.svg" },
+  { id: 2, name: "Continue with Google", icon: "/icon/google.svg" },
+  { id: 3, name: "Continue with Apple", icon: "/icon/apple.svg" },
+];
