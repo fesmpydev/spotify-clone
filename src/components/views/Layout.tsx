707ba166c905c41a84d5a9d68cd3909dc7b7f266
Fabio Sosa <fabio_sosapy@outlook.com>
2024-06-30 22:34:47 -0400

    Agregue Nav y Miniplayer para la visualizacion en todas las rutas hijas del root

--- src/components/views/Layout.tsx
+++ src/components/views/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Nav from "./Nav";
+import MiniPlayer from "../shared/MiniPlayer";
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <Nav />
+      <MiniPlayer />
       <Outlet />
     </>
   );
